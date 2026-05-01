185b171d14dd261e36a0f780b063ed77bb9f8dd4
LokeshLokii-VEL <lokeshlogii1511@gmail.com>
2026-05-01 15:17:20 +0530

    updated

--- script.js
+++ script.js
@@ -116,3 +116,15 @@ document.getElementById('contact-form').addEventListener('submit', function(e) {
             alert('❌ Failed! Try again.');
         });
 });
+
+
+function toggleMenu() {
+    const navLinks = document.querySelector('.nav-links');
+    navLinks.classList.toggle('active');
+}
+
+document.querySelectorAll('.nav-links a').forEach(link => {
+    link.addEventListener('click', () => {
+        document.querySelector('.nav-links').classList.remove('active');
+    });
+});
